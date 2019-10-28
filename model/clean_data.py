cc4945b18e64c1efee3f43cfea9c9ba7ea1b9b1a
Amit Lamichhane <Lamichhaneamit7@gmail.com>
2019-10-28 15:45:13 +1100

    model prepared

---
+++ model/clean_data.py
@@ -0,0 +1,30 @@
+import pandas as pd 
+
+
+"""
+changing x to 1 
+         0 to 2 
+         b to 0 
+         negative to 0 
+         positive to 1 
+"""
+def map_conditon(x):
+    if x =='x':
+        return 1 
+    elif x =='o':
+        return 2 
+    elif x =='b':
+        return 0
+    elif x=="negative":
+        return 0
+    else:
+        return 1
+
+def change_data(data):
+    data = data.applymap(map_conditon)
+    return data
+
+if __name__ =="__main__":
+    data = pd.read_csv('./../data/tic_tac_toe_dataset.csv')
+    data = change_data(data)
+    data.to_csv('./../data/model.csv', index= False)
---
+++ model/train.py
@@ -0,0 +1,116 @@
+
+import imp
+import pandas as pd 
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix
+from sklearn.externals import joblib
+from sklearn.externals.joblib import dump,load
+from sklearn import tree
+import graphviz 
+ 
+
+def save_models(model_name, file):
+   dump(model_name,"./{}".format(file))
+"""
+Four different classification model is going to be use and tested 
+Each model are saved for prediction using sklern.externals
+and some few probablistic classification
+"""
+"""
+"""
+def knn_model(feature, target):
+
+    #training with train test score 
+
+    
+    knn = KNeighborsClassifier()
+    ##grid search cross -fold
+    knn_params = {"n_neighbors":np.arange(1,25)}
+    knn_gscv = GridSearchCV(knn, knn_params,cv=10)
+    knn_gscv.fit(feature,target.values.ravel())
+    print(knn_gscv.best_params_)
+    print(knn_gscv.best_score_)
+
+    save_models(knn_gscv,"knn_model.joblib")
+
+
+def decision_tree(feature, target):
+    clf = DecisionTreeClassifier(random_state = 0 )
+    #clf = clf.fit(feature,target)
+    parameter_grid = {'max_depth': [1, 2, 3, 4, 5],
+                  'max_features': [1, 2, 3, 4]}
+
+    clf = GridSearchCV(clf,parameter_grid,cv=10)
+    clf =clf.fit(feature,target.values.ravel())
+    print(clf.best_params_)
+    print(clf.best_score_)
+
+
+    dot_data = tree.export_graphviz(clf, out_file=None) 
+    graph = graphviz.Source(dot_data) 
+    graph.render("ok") 
+    save_models(clf,"decision_tree.joblib")
+    
+
+def random_forest(feature, target):
+    pass
+
+def naive_bayes(feature, target):
+    
+    naive_model = GaussianNB()
+    naive_model.fit(feature,target)
+    ##grid search cross -fold
+    save_models(naive_model,"naive_bayes_model.joblib")
+
+def svm(feature, target):
+    pass
+
+
+def predict_using_model(x,y,filename= "./knn_model.joblib"):
+    model = load(filename)
+    y_pred = model.predict(x)
+    #print(y_pred)
+    #print(knn.predict_proba(x))    
+    #print(y_pred)
+    #print(model.predict_proba(x))
+    matrix = confusion_matrix(y , y_pred)
+    print({
+        "accuracy": float((matrix[0][0]+matrix[1][1])/(sum(matrix[0])+sum(matrix[1]))),
+        "no_correct" : int(matrix[0][0]),
+        "no_incorrect" : int(matrix[0][1]),
+        "yes_correct" : int(matrix[1][1]),
+        "yes_incorrect" : int(matrix[1][0])
+        })
+
+if __name__ == "__main__":
+    data = pd.read_csv("./../data/model.csv")
+    X, y = np.split(data, [-1], axis=1)
+    seed= 2000
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state = seed)
+    
+    ##knn_models
+    #knn_model(X_train,y_train)
+    #predict_using_model(X_test,y_test, filename="./knn_model.joblib")
+    #naive_bayes
+    #naive_bayes(X_train,y_train)
+    #predict_using_model(X_test,y_test, filename= "./naive_bayes_model.joblib")
+    #decision_tree very unstable
+    decision_tree(X_train,y_train)
+    predict_using_model(X_test,y_test,filename="./decision_tree.joblib")
+
+
+
+
+
+    
+    
+      
+
+    
